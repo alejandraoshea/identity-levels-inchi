7ac47a5cadc8fd53f149c97d5b5358afc8aa8c93
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2026-04-02 12:15:07 +0200

    added new route

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from backend.inchi.compare import compare_files
 from backend.inchi.determine_levels_id import InChi
 from backend.inchi.config_loader import load_config
 from backend.routes.inchi_comparison_routes import inchi_comparison_routes
+from backend.routes.generation_3d_routes import generation_3d_routes
 import tempfile
 import json
 from flask_cors import CORS
@@ -12,6 +13,7 @@ CORS(app)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 app.register_blueprint(inchi_comparison_routes)
+app.register_blueprint(generation_3d_routes)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
