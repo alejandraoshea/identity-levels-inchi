34fe678f73b591d3ffff53b8fadff9c537b3adf4
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2026-04-19 19:59:29 +0200

    refactor lipid analysis

--- backend/inchi/lipid_analysis.py
+++ backend/inchi/lipid_analysis.py
@@ -3,25 +3,10 @@ from functools import lru_cache
 from rdkit import Chem
 from rdkit.Chem import MolToSmiles
 from collections import Counter
+from backend.inchi.lipid_tail_extraction import TailExtractor
 
 class LipidAnalysis:
     CLASSYFIRE_URL = "https://classyfire.wishartlab.com/entities.json"
-
-    # SMARTS patterns used to detect polar lipid headgroups
-    HEAD_ANCHORS = {
-        "phosphate": Chem.MolFromSmarts("P(=O)(O)(O)"), #detects phospolipids
-        "phosphocholine": Chem.MolFromSmarts("P(=O)(O)(O)OCC[N+](C)(C)C"),
-        "phosphoethanolamine": Chem.MolFromSmarts("P(=O)(O)(O)OCCN"),
-        "phosphoserine": Chem.MolFromSmarts("P(=O)(O)(O)OCC(N)C(=O)O"),
-        "amine": Chem.MolFromSmarts("[NX3;H2,H1,H0;+0,+1]"), #detects nitrogen group like ethanolamine, choline, serine
-        "quaternary_amine": Chem.MolFromSmarts("[NX4+]"),
-        "amide": Chem.MolFromSmarts("C(=O)N"), #detects sphingolipid linkages
-        "sugar_ring": Chem.MolFromSmarts("C1OC(O)C(O)C(O)C1O"), #detects glycolipid heads
-        "glycerol": Chem.MolFromSmarts("OCC(O)CO"),
-        "carboxyl": Chem.MolFromSmarts("C(=O)[O;H,-]"), #detects FA headgroups
-        "sulfonate": Chem.MolFromSmarts("S(=O)(=O)O"),
-        "sterol_core": Chem.MolFromSmarts("C1CCC2C(C1)CCC3C2CCC4C3CCCC4"),
-    }
 
     #min chain length to consider it a FA
     MIN_TAIL_CARBONS = 6
@@ -32,12 +17,14 @@ class LipidAnalysis:
             return False
 
         has_chain = LipidAnalysis.has_long_carbon_chain(mol, min_len=8)
-        head_atoms = LipidAnalysis.detect_head_atoms(mol)
+        head_atoms = TailExtractor.detect_head_atoms(mol)
         ester, amide, ether = LipidAnalysis.count_lipid_linkages(mol)
 
         # Strong lipid signal
-        if has_chain and len(head_atoms) > 0:
-            return True
+        if len(head_atoms) > 0:
+            tails = TailExtractor.extract_tails(mol)
+            if len(tails) >= 1:
+                return True
 
         # Fatty acids
         if has_chain and mol.HasSubstructMatch(LipidAnalysis.HEAD_ANCHORS["carboxyl"]):
@@ -168,190 +155,6 @@ class LipidAnalysis:
 
         return ester, amide, ether
 
-    # STEP 1 remove cis/trans stereochemistry
-    @staticmethod
-    def remove_cis_trans(mol):
-        mol = Chem.Mol(mol)
-        for bond in mol.GetBonds():
-            if (
-                bond.GetBondType() == Chem.BondType.DOUBLE
-                and bond.GetBeginAtom().GetAtomicNum() == 6
-                and bond.GetEndAtom().GetAtomicNum() == 6):
-                    bond.SetStereo(Chem.BondStereo.STEREONONE) #rdkit recomputes stereochemistry 
-                
-        Chem.AssignStereochemistry(mol, cleanIt=True, force=True)
-        return mol
-
-    # STEP 2 detect head atoms via SMARTS anchors
-    @staticmethod
-    def detect_head_atoms(mol):
-        """
-        Find atoms belonging to the lipid headgroup
-        For each SMARTS pattern: find matches and store atoms
-        """
-        head_atoms = set()
-        for smarts in LipidAnalysis.HEAD_ANCHORS.values():
-            matches = mol.GetSubstructMatches(smarts)
-
-            for match in matches:
-                head_atoms.update(match)
-
-        return head_atoms
-
-    @staticmethod
-    def walk_chain(start_atom, coming_from):
-            stack = [(start_atom, None)]
-            visited = set()
-
-            carbons = 0
-            double_bonds = 0
-            oxygens = 0
-
-            db_positions = []
-            o_positions = []
-
-            position = 0  # relative position along chain
-
-            while stack:
-                atom, prev = stack.pop()
-                idx = atom.GetIdx()
-
-                if idx in visited:
-                    continue
-                visited.add(idx)
-
-                if atom.GetSymbol() == "C":
-                    carbons += 1
-                    position += 1
-                elif atom.GetSymbol() == "O":
-                    oxygens += 1
-                    o_positions.append(position)
-
-                for bond in atom.GetBonds():
-                    nbr = bond.GetOtherAtom(atom)
-
-                    if prev is not None and nbr.GetIdx() == prev:
-                        continue
-
-                    # detect C=C double bonds
-                    if (
-                        bond.GetBondType() == Chem.rdchem.BondType.DOUBLE
-                        and atom.GetSymbol() == "C"
-                        and nbr.GetSymbol() == "C"
-                    ):
-                        double_bonds += 1
-                        db_positions.append(position)
-
-                    if nbr.GetSymbol() in ["C", "O"]:
-                        stack.append((nbr, atom.GetIdx()))
-
-            return {
-                "C": carbons,
-                "DB": double_bonds,
-                "O": oxygens,
-                "DB_positions": tuple(sorted(db_positions)),
-                "O_positions": tuple(sorted(o_positions)),
-                "atoms": tuple(sorted(visited)),
-            }
-    
-    # STEP 3 extract tails via graph traversal
-    @staticmethod
-    def extract_tails(mol):
-        #Detect and exclude head atoms first
-        head_atoms = LipidAnalysis.detect_head_atoms(mol)
-        
-        visited_global = set()
-        visited_global.update(head_atoms)
-        tails = []
-
-        def walk_chain(start_atom_idx):
-            visited = set()
-            stack = [(start_atom_idx, None)]
-
-            carbons = 0
-            double_bonds = 0
-            oxygens = 0
-            db_positions = []
-            o_positions = []
-
-            position = 0
-
-            while stack:
-                atom_idx, parent = stack.pop()
-
-                if atom_idx in visited or atom_idx in head_atoms:
-                    continue
-
-                visited.add(atom_idx)
-                atom = mol.GetAtomWithIdx(atom_idx)
-
-                if atom.GetAtomicNum() == 6:
-                    carbons += 1
-                    position += 1
-
-                    for bond in atom.GetBonds():
-                        neighbor = bond.GetOtherAtom(atom)
-                        neighbor_idx = neighbor.GetIdx()
-
-                        if neighbor_idx == parent:
-                            continue
-                        
-                        # stop at head atoms
-                        if neighbor_idx in head_atoms:
-                            continue
-
-                        # continue carbon chain
-                        if neighbor.GetAtomicNum() == 6:
-                            if bond.GetBondType() == Chem.BondType.DOUBLE:
-                                double_bonds += 1
-                                db_positions.append(position)
-
-                            stack.append((neighbor_idx, atom_idx))
-
-                        # oxygen attached (but not in head)
-                        elif neighbor.GetAtomicNum() == 8:
-                            oxygens += 1
-                            o_positions.append(position)
-
-            if carbons >= 6:  # filter small fragments
-                return {
-                    "C": carbons,
-                    "DB": double_bonds,
-                    "O": oxygens,
-                    "DB_positions": tuple(sorted(db_positions)),
-                    "O_positions": tuple(sorted(o_positions)),
-                    "atoms": tuple(sorted(visited)),
-                }
-
-            return None
-
-        # start from all carbons NOT in the headgroup
-        for atom in mol.GetAtoms():
-            if atom.GetAtomicNum() == 6:
-                idx = atom.GetIdx()
-
-                if idx in visited_global:
-                    continue
-
-                tail = walk_chain(idx)
-
-                if tail:
-                    tails.append(tail)
-                    visited_global.update(tail["atoms"])
-
-        # deduplicate by atoms
-        unique = []
-        seen_atoms = set()
-
-        for t in tails:
-            atom_key = t["atoms"]
-
-            if atom_key not in seen_atoms:
-                seen_atoms.add(atom_key)
-                unique.append(t)
-
-        return unique
-    
     def tail_sig_levelB(t):
         return (
             t["C"],
@@ -404,32 +207,3 @@ class LipidAnalysis:
             return None
 
         return (carbons, double_bonds)
-
-    # STEP 5: build lipid signature
-    @staticmethod
-    def lipid_signature(mol):
-        head_atoms = LipidAnalysis.detect_head_atoms(mol)
-
-        if not head_atoms:
-            return None
-
-        tails = LipidAnalysis.extract_tails(mol)
-        tail_sigs = []
-
-        for tail in tails:
-            sig = (tail["C"], tail["DB"])
-            if sig:
-                tail_sigs.append(sig)
-
-        return tuple(sorted(tail_sigs))
-
-    # STEP 6 compare molecules
-    @staticmethod
-    def equal_ignore_double_bond_position(mol1, mol2):
-        sig1 = LipidAnalysis.lipid_signature(mol1)
-        sig2 = LipidAnalysis.lipid_signature(mol2)
-
-        if sig1 is None or sig2 is None:
-            return Chem.MolToSmiles(mol1, canonical=True) == Chem.MolToSmiles(mol2, canonical=True)
-
-        return sig1 == sig2
